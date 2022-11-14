e97b3f09e93390099872028f9463918c8eb62cf4
MSI <MSI@DESKTOP-367NOSU>
2022-11-14 17:39:19 +1000

    functions search/insert (not overfllow)

--- BPTree/BPTree.h
+++ BPTree/BPTree.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef BPTree
 #define BPTree
 #include <iostream>
@@ -11,16 +10,15 @@ public:
 	bool isLeaf;
 	int size;
 	int* key;
-	Node** pointer;
-	Node* parent;
+	Node** children;
+	Node* parents;
 
 	Node() {
 		key = new int[Node::max - 1];
-		pointer = new Node * [Node::max];
+		children = new Node * [Node::max];
 		this->isLeaf = false;
+		parents = nullptr;
 	}
-
-	void split();
 	friend class BPlusTree;
 };
 int Node::max;
@@ -28,24 +26,81 @@ int Node::min;
 
 class BPlusTree {
 
-public:	
+public:
+	Node* root = nullptr;
 	int degree;
 	BPlusTree(int m) {
 		this->degree = m;
 		this->root = nullptr;
 
-		Node::max = m;
+		Node::max = m-1;
 		Node::min = m / 2 - 1;
 	}
 
-	void insert_key(int k);
-	void delete_key(int);
-	int* search_key(int);	
-	void print(Node*);
+	void insert_key(int k) {
+		Node* current;
+		if (root == nullptr) {
+			root = new Node();
+			root->key[0] = k;
+			root->isLeaf = true;
+			root->size = 1;
+			current = root;
+		}
 
-private:
-	Node* search_leaf(int k);
-	Node* root = nullptr;
+		else {
+			current = find_leaf(k);
+
+			//если лист не переполнен
+			if (current->size < Node::max) {
+				int place = find_index(current->key, k, current->size);
+				
+				for (int i = (current->size); i > place; i--) {
+					current->key[i] = current->key[i - 1];
+				}
+				current->key[place] = k;
+				current->size++;
+			}
+
+			//если лист переполнен
+			else {
+				split(current, k, Node::max / 2 + 1);
+			}
+		}
+	}
+	void delete_key(int);
+
+	bool find_key(int k) {
+		Node* current = find_leaf(k);
+		int i = find_index(current->key, k, current->size);
+		if (current->key[i] == k) return true;
+		else return false;
+	};
+	int find_index(int arr[], int k, int size) {
+		if (k >= arr[size-1]) return size;
+		else {
+			for (int i = 0; i < size; i++) {
+				if (k < arr[i]) {
+					return i;
+				}
+			}
+		}
+
+	}
+
+	Node* find_leaf(int k) {
+		Node* current = root;
+		
+		while (!current->isLeaf) {
+			current = current->children[find_index(current->key, k, current->size)];
+		}
+			return current;
+		
+	}
+
+	void split(Node * current, int k, int point) {
+		
+	}
+	void print(Node*);
 };
 
 
